b7f2d43f401c3a60ed6690e311cd5b157fc5e995
Bruno Martins <bgcngm@gmail.com>
2020-02-25 20:55:33 +0000

    sepolicy: Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
    Change-Id: Ibb700942ef0c383faad7b6bf461206a957bd409c
    Co-authored-by: Adrian DC <radian.dc@gmail.com>
    Co-authored-by: Arne Coucheron <arco68@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -23,3 +23,6 @@ BOARD_SEPOLICY_DIRS += \
     device/custom/sepolicy/common/dynamic \
     device/custom/sepolicy/common/vendor
 endif
+
+# Selectively include legacy rules defined by the products
+-include device/custom/sepolicy/legacy-common/sepolicy.mk
