eb438c5faff58def358473bda2cd8a279b6126ec
Alexandre Rangel <mamutal91@gmail.com>
2021-03-10 00:15:55 -0300

    mediatek: Fix path
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: Id821c3fdb05eeb2716c5dcc77457eae37a33115f

--- mediatek/sepolicy.mk
+++ mediatek/sepolicy.mk
@@ -11,10 +11,10 @@ endif
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/mediatek/dynamic \
-    device/lineage/sepolicy/mediatek/system
+    device/custom/sepolicy/mediatek/dynamic \
+    device/custom/sepolicy/mediatek/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/mediatek/dynamic \
-    device/lineage/sepolicy/mediatek/vendor
+    device/custom/sepolicy/mediatek/dynamic \
+    device/custom/sepolicy/mediatek/vendor
 endif
