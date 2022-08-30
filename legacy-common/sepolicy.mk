05e5dc280dc35cd2dc98143b5e1ce0f32ba41d7f
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:15:03 +0300

    Remove legacy camera HAL1 sepolicy
    Change-Id: I3ef0923445768b54095dcf79dc71e3ec408c1ca2

--- legacy-common/sepolicy.mk
+++
@@ -1,9 +0,0 @@
-#
-# This policy configuration will be used by select legacy products that
-# inherit from PixelExperience
-#
-
-ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/custom/sepolicy/legacy-common/public/legacy-camera-hal1
-endif
