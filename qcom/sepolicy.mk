301153d8c133f7418cdf3a505cbff9dfaa98a658
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-06-22 18:49:50 +0530

    sepolicy: perf: Add vendor prefix
    And adapt the files for qcom-common and AOSP seliux
    
    Change-Id: Ieabcad8fe25b9cb6898ce43d95761fda8c5984ae
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -20,6 +20,14 @@ else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/custom/sepolicy/qcom/dynamic \
     device/custom/sepolicy/qcom/vendor
+
+ifeq "$(wildcard device/qcom/common)" ""
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/custom/sepolicy/qcom/vendor-perf
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/custom/sepolicy/qcom/vendor-perf-qcom
+endif
 endif
 
 ifneq ($(filter msm8960 msm8226 msm8610 msm8974 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
